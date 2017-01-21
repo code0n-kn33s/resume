545fac87fcffee94dba0c0561bb6cf54e3e4b32d
resumesite <nativecase@gmail.com>
2017-01-21 03:44:04 +0200

    resume v2.1

--- dist/js/common.js
+++ dist/js/common.js
@@ -316,4 +316,64 @@ jQuery(document).ready(function($) {
 
 //check if an element exists in the DOM
 jQuery.fn.exists = function() {
-    return this.length > 0; }
+        return this.length > 0;
+    }
+    // копирование имейл
+var copyEmailBtn = document.querySelector('.js-emailcopybtn');
+copyEmailBtn.addEventListener('click', function(event) {
+    // Выборка ссылки с электронной почтой
+    var emailLink = document.querySelector('.js-emaillink');
+    var range = document.createRange();
+    range.selectNode(emailLink);
+    window.getSelection().addRange(range);
+
+    try {
+        // Теперь, когда мы выбрали текст ссылки, выполним команду копирования
+        var successful = document.execCommand('copy');
+        var msg = successful ? 'successful' : 'unsuccessful';
+        console.log('Copy email command was ' + msg);
+    } catch (err) {
+        console.log('Oops, unable to copy');
+    }
+
+    // Снятие выделения
+    window.getSelection().removeAllRanges();
+});
+//copy button animation
+var buttons7Click = Array.prototype.slice.call(document.querySelectorAll('#btn-click button')),
+    buttons9Click = Array.prototype.slice.call(document.querySelectorAll('button.btn-8g')),
+    totalButtons7Click = buttons7Click.length,
+    totalButtons9Click = buttons9Click.length;
+
+buttons7Click.forEach(function(el, i) {
+    el.addEventListener('click', activate, false);
+});
+buttons9Click.forEach(function(el, i) {
+    el.addEventListener('click', activate, false);
+});
+
+function activate() {
+    var self = this,
+        activatedClass = 'btn-activated';
+
+    if (classie.has(this, 'btn-7h')) {
+        // if it is the first of the two btn-7h then activatedClass = 'btn-error';
+        // if it is the second then activatedClass = 'btn-success'
+        activatedClass = buttons7Click.indexOf(this) === totalButtons7Click - 2 ? 'btn-error' : 'btn-success';
+    } else if (classie.has(this, 'btn-8g')) {
+        // if it is the first of the two btn-8g then activatedClass = 'btn-success3d';
+        // if it is the second then activatedClass = 'btn-error3d'
+        activatedClass = buttons9Click.indexOf(this) === totalButtons9Click - 2 ? 'btn-success3d' : 'btn-error3d';
+    }
+
+    if (!classie.has(this, activatedClass)) {
+        classie.add(this, activatedClass);
+        setTimeout(function() {
+            classie.remove(self, activatedClass)
+        }, 1000);
+    }
+}
+
+document.querySelector('.btn-7i').addEventListener('click', function() {
+    classie.add(document.querySelector('#trash-effect'), 'trash-effect-active');
+}, false);
